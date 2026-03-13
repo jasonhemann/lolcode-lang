6bea890276731e74061f035fbb0535923195089a
Jason Hemann <jason.hemann@gmail.com>
2026-03-13 17:40:22 -0400

    Remove unused split-until helper module

--- src/lolcode/rkt-extras.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket/base
-
-(require racket/list)
-
-(provide split-until)
-
-;; Dual of `splitf-at`: split at the first element satisfying `stop?`.
-(define (split-until xs stop?)
-  (splitf-at xs (lambda (x) (not (stop? x)))))
